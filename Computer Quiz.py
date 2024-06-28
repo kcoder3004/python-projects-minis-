7d41c9de6834b8e5f79fd8029e75944218edb09b
cashbooda <kjprogrammerpc@gmail.com>
2024-06-28 19:17:45 -0400

    Created Computer Quiz.py
    try this computer quiz to test out YOUR knowledge of computers!

---
+++ Computer Quiz.py
@@ -0,0 +1,46 @@
+score = 0
+print("Welcome to my Computer Quiz! ")
+
+playing = input("Do you want to play? \n")
+
+if playing.lower() != "yes":
+    print("Okay See Ya!")
+    quit()
+print("Okay, Lets Play! :D")
+answer = input("What does CPU stand for? \n")
+if answer.lower() == "central processing unit":
+    print("Correct! ")
+    score += 1
+else:
+    print("Wrong! ")
+
+answer = input("What does GPU stand for? \n")
+if answer.lower() == "graphics processing unit":
+    print("Correct! ")
+    score += 1
+else:
+    print("Wrong! ")
+
+answer = input("What does RAM stand for? \n")
+if answer.lower() == "random access memory":
+    print("Correct! ")
+    score += 1
+else:
+    print("Wrong! ")
+
+answer = input("What does PSU stand for? \n")
+if answer.lower() == "power supply":
+    print("Correct! ")
+    score += 1
+else:
+    print("Wrong! ")
+
+if score < 2:
+    performance = "Poorly"
+elif score == 2:
+    performance = "Average"
+else:
+    performance = "Excellent"
+
+percentage = score/4 * 100
+print(f"You got {score}/4 questions right! You did {performance} with {percentage}% accuracy!")
